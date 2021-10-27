b6bcb1d5f93bdf72f149a0abf72cc29a9efdb70f
Wilian-N-Silva <wilian.silva209@gmail.com>
2021-10-27 03:44:15 -0300

    ADD: 'Search' (list), 'New' and 'Delete' endpoint functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,316 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+const String baseUrl = 'http://10.0.2.2:8080/php-api-to-do-list';
+enum taskMenuItems { edit, delete }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+class User {
+  User(
+    this.id,
+    this.name,
+    this.email,
+    this.token,
+  );
+
+  final int id;
+  final String name;
+  final String email;
+  final String token;
+
+  User.fromJson(Map json)
+      : id = json['id'],
+        name = json['name'],
+        email = json['email'],
+        token = json['token'];
+}
+
+class Task {
+  final int id;
+  final int userId;
+  final String name;
+  final String date;
+  final int realized;
+
+  Task(
+    this.id,
+    this.userId,
+    this.name,
+    this.date,
+    this.realized,
+  );
+
+  Task.fromJson(Map json)
+      : id = json['id'],
+        userId = json['userId'],
+        name = json['name'],
+        date = json['date'],
+        realized = json['realized'];
+}
+
+class API {
+  // TASKS
+
+  // SEARCH
+  static Future getTasks() async {
+    Map<String, String> requestHeaders = {
+      'Content-type': 'application/json',
+      'Authorization': '123'
+    };
+
+    return await http.post(
+      Uri.parse('$baseUrl/api/task/search/'),
+      headers: requestHeaders,
+    );
+  }
+
+  // NEW
+  static Future newTask(String taskname) async {
+    Map<String, String> requestHeaders = {
+      'Content-type': 'application/json',
+      'Authorization': '123'
+    };
+    return await http.post(
+      Uri.parse('$baseUrl/api/task/new/'),
+      headers: requestHeaders,
+      body: jsonEncode({
+        "name": taskname,
+      }),
+    );
+  }
+
+  // DELETE
+  static Future deleteTask(int taskId) async {
+    Map<String, String> requestHeaders = {
+      'Content-type': 'application/json',
+      'Authorization': '123'
+    };
+    return await http.delete(
+      Uri.parse('$baseUrl/api/task/delete/'),
+      headers: requestHeaders,
+      body: jsonEncode({
+        "id": taskId,
+      }),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const TaskList(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TaskList extends StatefulWidget {
+  const TaskList({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  bool isLoading = true;
+  List taskList = [];
+
+  final _newTaskFormKey = GlobalKey<FormState>();
+  final TextEditingController _newTaskName = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getTaskList();
+  }
+
+  _getTaskList() {
+    isLoading = true;
+
+    API.getTasks().then((response) {
+      if (response.statusCode == 200) {
+        var body = json.decode(response.body);
+        setState(() {
+          taskList = body[0] == null
+              ? []
+              : body.map((model) => Task.fromJson(model)).toList();
+          isLoading = false;
+        });
+      }
     });
+  }
+
+  _taskList() {
+    if (!isLoading) {
+      return taskList.isEmpty
+          ? const Center(child: Text('Não há tarefas criadas'))
+          : ListView.separated(
+              itemCount: taskList.length,
+              physics: const BouncingScrollPhysics(),
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+              itemBuilder: (BuildContext context, int index) {
+                return _taskTile(context, index);
+              },
+            );
+    } else {
+      return const Center(child: CircularProgressIndicator());
+    }
+  }
+
+  _taskTile(BuildContext context, index) {
+    final task = taskList[index];
+    return Dismissible(
+      key: Key(task.id.toString()),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) async {
+        var checkTemp = taskList[index];
+        setState(() {
+          taskList.removeAt(index);
+        });
+
+        final response = await API.deleteTask(task.id);
+
+        var body = json.decode(response.body);
+
+        if (response.statusCode == 200 &&
+            body['message'] != 'Task deleted Successfully') {
+          setState(() {
+            taskList.add(checkTemp);
+          });
+        }
+      },
+      background: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      child: ListTile(
+        title: Text(task.name),
+        leading: task.realized == 0
+            ? const Icon(Icons.check_box_outline_blank)
+            : const Icon(Icons.check_box_outlined),
+      ),
+    );
+  }
+
+  _saveTask() async {
+    if (_newTaskFormKey.currentState!.validate()) {
+      final response = await API.newTask(_newTaskName.text);
+
+      if (response.statusCode == 200) {
+        _newTaskName.clear();
+        Navigator.pop(context, true);
+        _getTaskList();
+      }
+    }
+  }
+
+  _newTaskDialog() {
+    return AlertDialog(
+      title: const Text('Nova Tarefa'),
+      content: Form(
+        key: _newTaskFormKey,
+        child: TextFormField(
+          controller: _newTaskName,
+          decoration: const InputDecoration(
+            label: Text('Título'),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Preencha o título da tarefa';
+            }
+            return null;
+          },
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: const Text('Cancelar'),
+          onPressed: () {
+            _newTaskName.clear();
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(child: const Text('Salvar'), onPressed: () => _saveTask()),
+      ],
+      actionsAlignment: MainAxisAlignment.spaceAround,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Tarefas'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      floatingActionButton: !isLoading
+          ? FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => _newTaskDialog(),
+                );
+              },
+            )
+          : null,
+      body: _taskList(),
+    );
+  }
+}
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({Key? key}) : super(key: key);
+
+  @override
+  _UserProfileState createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Perfil'),
+        actions: [
+          TextButton(
+            child: const Text(
+              'Salvar',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              print('test');
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: [],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
